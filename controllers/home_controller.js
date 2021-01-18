c3b8024d0accecbdde1065a7125ffff74c4ab209
Shashwat Kumar Singh <73544122+harryalbrus272@users.noreply.github.com>
2021-01-18 23:53:49 +0530

    included posts and user associated with the post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,26 @@
+const { populate } = require('../models/post');
+const Post = require('../models/post');
+const Posts = require('../models/post');
 module.exports.home = function(req, res){
-    console.log(req.cookies);
+    //console.log(req.cookies);
     //changing cookie in the response
-    res.cookie('user_id',30);
-    return res.render('home',{
-        title: "Home"
-    });
+    //res.cookie('user_id',30);
+    //function returning all the posts
+    /*Post.find({}, function(err,posts){
+        return res.render('home',{
+            title: "NeroSocial | Home",
+            //passing on all he posts
+            posts: posts
+        });
+    });*/
+    //prepopulating the database of each user for each posts
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title: "NeroSocial | Home",
+            //passing on all he posts
+            posts: posts
+        });
+    });    
 }
 module.exports.actionBar = function(req,res){
     return res.end('<h1>Actionbar is up and Running</h1>');
--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,5 @@ router.get('/actionbar', homeController.actionBar);
 router.use('/users',require('./users'));
 router.use('/messenger',require('./messenger'));
 router.use('/channels',require('./channels'));
+router.use('/posts', require('./posts'));
 module.exports = router;
