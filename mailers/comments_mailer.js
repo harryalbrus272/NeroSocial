ceb9a67ee859620b9395b5d47829ec3bbf5b9f08
Shashwat Kumar Singh <73544122+harryalbrus272@users.noreply.github.com>
2021-02-09 15:38:44 +0530

    added ejs template to mailer job

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -2,13 +2,14 @@ const nodeMailer = require('../config/nodemailer');
 
 //this is another way of exporting a method
 exports.newComment = (comment) => {
+    let htmlString = nodeMailer.renderTemplate({comment:comment}, '/comments/new_comment.ejs');
     console.log('inside new comment mailer', comment);
     //sendMail is predefined function with parametere
     nodeMailer.transporter.sendMail({
         from: 'shashwatksingh.27@gmail.com',
         to: comment.user.email,
         subject: "New Comment Pulished",
-        html: '<h1>Comment has been published.</h1>'
+        html: htmlString
     }, (error,info) => {
         if(error){
             console.log('Error in sending the mail', error);
